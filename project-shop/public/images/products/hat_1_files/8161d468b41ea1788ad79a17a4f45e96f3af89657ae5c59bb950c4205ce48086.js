edf46a4fef07eabb08e7e39a0462d17736828a0c
Yana Shepielieva <yanashepeleva@MacBook-Air-Yana.local>
2025-12-28 21:51:49 +0100

    Remove files

--- project-shop/public/images/products/hat_1_files/8161d468b41ea1788ad79a17a4f45e96f3af89657ae5c59bb950c4205ce48086.js
+++
@@ -1,5 +0,0 @@
-window.optimizely.push({type:"load",data:{changes:[{"dependencies": [], "type": "custom_code", "id": "CA448364-B89B-4F71-842A-AD9204A44C1E", "value": function($){var utils = optimizely.get('utils');
-utils.waitForElement('#delivery-and-returns-portal').then(function() {
-	document.querySelector("[data-testid='deliveryAndReturns__freeDelivery']").remove();
-});
-}}]}});
